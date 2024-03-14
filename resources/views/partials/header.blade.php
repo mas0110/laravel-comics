09681c79f4256740c8a45245d83eda6f2976347e
mas0110 <masoudmohammed13@gmail.com>
2024-03-14 14:36:41 +0100

    sezione header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+  <div class="div-header">
+      <figure>
+         {{-- <img src="../assets/img/dc-logo.png" alt=""> --}}
+         <img src="{{ Vite:: asset('resources/img/dc-logo.png') }}" alt="">
+      </figure>
+      <div>
+          <ul>
+              <li>CHARACTERS</li>
+              <li>COMICS</li>
+              <li>MOVIES</li>
+              <li>TV</li>
+              <li>GAMES</li>
+              <li>COLLECTION</li>
+              <li>VIDEOS</li>
+              <li>FANS</li>
+              <li>NEWS</li>
+              <li>SHOP</li>
+          </ul>
+      </div>
+  </div>
